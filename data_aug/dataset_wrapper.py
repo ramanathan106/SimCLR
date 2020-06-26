875dcf022a66e38ae6393bbcf59842da40e9b987
ramanathan106 <ramanathan0811@gmail.com>
2020-06-26 13:47:39 +0530

    changed super class

--- data_aug/dataset_wrapper.py
+++ data_aug/dataset_wrapper.py
@@ -4,7 +4,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 import torchvision.transforms as transforms
 from data_aug.gaussian_blur import GaussianBlur
 from torchvision import datasets
-from torch.utils.data.dataset import Dataset
+from torchvision.datasets.vision import VisionDataset
 import pandas as pd
 import cv2
 
@@ -75,7 +75,7 @@ class SimCLRDataTransform(object):
         return xi, xj
 
 
-class SimpleDataset(Dataset):
+class SimpleDataset(VisionDataset):
     def __init__(self, csv_file, transform=None):
         self.data = pd.read_csv(csv_file).to_dict("records")
         self.transform = transform
